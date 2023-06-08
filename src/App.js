2f949f3e5ed82e5fdb6bbd434d582c21d058cb54
choco <choco.luo@playerauctions.com>
2023-06-08 12:10:59 +0800

    original

--- src/App.js
+++ src/App.js
@@ -1,82 +1,109 @@
-import './app.css'
-import { useEffect, useState } from "react";
+import "./app.css";
+import React, { useEffect, useState } from "react";
 import {
-  RecordVoiceOverOutlined, 
+  RecordVoiceOverOutlined,
   VoiceOverOff,
-  SettingsOutlined
-} from '@mui/icons-material';
+  SettingsOutlined,
+} from "@mui/icons-material";
 import { useSpeechSynthesis } from "react-speech-kit";
-import Dialog from './Dialog'
-
+import Dialog from "./Dialog";
+import { questionData } from "./questionData";
 const App = () => {
-  const [showSpeechSettings, setShowSpeechSettings] = useState(false)
-  const [highlightedText, setHighlightedText] = useState('')
+  const [showSpeechSettings, setShowSpeechSettings] = useState(false);
+  const [highlightedText, setHighlightedText] = useState("");
   const [voiceIndex, setVoiceIndex] = useState(null);
   const [pitch, setPitch] = useState(1);
   const [rate, setRate] = useState(1);
 
   const onEnd = () => {
-    setHighlightedText('')
-  }
-  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({onEnd})
+    setHighlightedText("");
+  };
 
-  const voice = voices[voiceIndex] || null
+  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({
+    onEnd,
+  });
+
+  const voice = voices[voiceIndex] || null;
 
   const handleTextSet = () => {
-    const text = window.getSelection().toString()
-    if(text !== '') setHighlightedText(text)
-  }
+    const text = window.getSelection().toString();
+    if (text !== "") setHighlightedText(text);
+  };
 
-  // setting highlightedText state when text has been highlighted
   useEffect(() => {
-    document.addEventListener('mouseup', handleTextSet)
+    document.addEventListener("mouseup", handleTextSet);
     return () => {
-      document.removeEventListener('mouseup', handleTextSet)
+      document.removeEventListener("mouseup", handleTextSet);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (supported && speaking && highlightedText === "") {
+      speak({ text: document.body.innerText, voice, rate, pitch });
     }
-  }, [])
+  }, [highlightedText, speak, supported, speaking, voice, rate, pitch]);
+
+
 
   return (
     <div className="app">
-      <h1>Text to Speech</h1>
-      <span>Highligh text and click the speack icon to listen to highlighted text</span>
+      <h1>Write From Dictation</h1>
+      <span>
+        6月全球预测
+      </span>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-        software like Aldus PageMaker including versions of Lorem Ipsum.
+        {questionData.map((question) => (
+          <React.Fragment key={question.id}>
+            <strong>{question.title}:</strong> {question.text}
+            <br />
+            {question.textEn}
+            <br />
+          </React.Fragment>
+        ))}
       </p>
 
-      {supported && 
+      {supported && (
         <div className="speechMenu">
-          {!speaking
-            ? <RecordVoiceOverOutlined onClick={() => speak({ text: highlightedText, voice, rate, pitch})}/>
-            : <VoiceOverOff onClick={cancel}/>
-          }
-          <SettingsOutlined onClick={() => setShowSpeechSettings(true)}/>
+          {!speaking ? (
+            <RecordVoiceOverOutlined
+              onClick={() =>
+                speak({
+                  text: highlightedText || document.body.innerText,
+                  voice,
+                  rate,
+                  pitch,
+                })
+              }
+            />
+          ) : (
+            <VoiceOverOff onClick={cancel} />
+          )}
+          <SettingsOutlined onClick={() => setShowSpeechSettings(true)} />
         </div>
-      }
+      )}
 
-      <Dialog open={showSpeechSettings} onClose={() => setShowSpeechSettings(false)}>
-        <div className='speechSettings'>
-          {/* VOices -- browser dependent */}
+      <Dialog
+        open={showSpeechSettings}
+        onClose={() => setShowSpeechSettings(false)}
+      >
+        <div className="speechSettings">
           <select
             name="voice"
-            value={voiceIndex || ''}
+            value={voiceIndex || ""}
             onChange={(e) => {
-              setVoiceIndex(e.target.value)
+              setVoiceIndex(e.target.value);
             }}
           >
             {voices.map((option, index) => (
               <option key={option.voiceURI} value={index}>
-                {`${option.lang} - ${option.name} ${ option.default ? '- Default' : ''}`}
+                {`${option.lang} - ${option.name} ${option.default ? "- Default" : ""
+                  }`}
               </option>
             ))}
           </select>
-          <div className='rangeContainer'>
+          <div className="rangeContainer">
             <div>
-              <label htmlFor="rate">Rate: </label>
+              <label htmlFor="rate">Rate:</label>
               <span>{rate}</span>
             </div>
             <input
@@ -90,9 +117,9 @@ const App = () => {
               }}
             />
           </div>
-          <div className='rangeContainer'>
+          <div className="rangeContainer">
             <div>
-              <label htmlFor="pitch">Pitch: </label>
+              <label htmlFor="pitch">Pitch:</label>
               <span>{pitch}</span>
             </div>
             <input
@@ -109,8 +136,8 @@ const App = () => {
           </div>
         </div>
       </Dialog>
-    </div>    
-  )
-}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/questionData.js
@@ -0,0 +1,33 @@
+export const questionData = [
+    {
+      id: 1,
+      title: "929",
+      text: "很明显，国家贸易体系是一件好事",
+      textEn: "it is clear that national trading system is a good thing",
+    },
+    {
+      id: 2,
+      title: "528",
+      text: "目前公司的发展是不可预测的",
+      textEn: "currently the growth of the company is unpredictable",
+    },
+    {
+      id: 3,
+      title: "826",
+      text: "计算机对学生来说是一个很好的知识来源",
+      textEn: "computer is a great source of knowledge for the students",
+    },
+    {
+      id: 4,
+      title: "814",
+      text: "我们正以非常快的速度前进",
+      textEn: "we are moving at a very fast pace",
+    },
+    {
+      id: 5,
+      title: "371",
+      text: "时间表将在早上公布在网站上",
+      textEn: "the timetable will be posted on the website in the morning",
+    },
+  ];
+  
